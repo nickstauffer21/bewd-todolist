a8b1082ab4dd9cd918703c7e1fe953746eaa7f9c
Nick Stauffer <nickstauffer456@gmail.com>
2023-12-29 20:57:04 -0500

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'homepage#index'
 
   # Add routes below this line
+  get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  delete 'tasks/:id' => 'tasks#destroy'
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+  put '/tasks/:id/mark_active' => 'tasks#mark_active'
 
   # Add routes below above line
 
